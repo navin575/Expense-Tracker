7ebb955d7aaef797af0b54a403ca6148c5ad53fa
navin575 <navinmohan575@gmail.com>
2026-06-25 13:41:35 +0530

    fix: add root route, cross-origin access, and database configurations

--- expense-tracker-backend-main/expense-tracker-backend-main/server.js
+++ expense-tracker-backend-main/expense-tracker-backend-main/server.js
@@ -33,4 +33,7 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send("Backend Server is Running Successfully!");
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
